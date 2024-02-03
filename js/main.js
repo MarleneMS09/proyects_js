81c1027b85963e85939d9f96d942f8640891ed2d
MarleneMS09 <marlenemsanchez05@gmail.com>
2024-02-02 22:41:39 -0600

    update style & examples

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let js = 'amazing';
+if (js === 'amazing') alert ('Javascript is fun');
+alert (js);
+
+let firstname = 'Armando';
+let lastname = 'Perez'
+let fullname = (`${firstname} ${lastname}`); 
+
